f266a6cacf94c3d4df43762291e96d3ab841602e
Michael Andersen <m.andersen@cs.berkeley.edu>
2015-05-26 20:54:22 -0700

    Adjust example a bit

--- example1/main.go
+++ example1/main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 
+	"github.com/immesys/bw2/objects"
 	"github.com/immesys/bw2bind"
 )
 
 func main() {
-	_, err := bw2bind.Connect("127.0.0.1:28589")
+	bw, err := bw2bind.Connect("127.0.0.1:28589")
 	if err != nil {
 		fmt.Println("Got error:", err)
+		return
 	}
 
 	//Create an entity for ourselves
-
+	eVK, keyblob, err := bw.CreateEntity(&bw2bind.CreateEntityParams{
+		Comment: "for the win",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Entity created: ", eVK)
+	fmt.Println("Keyfile: ", keyblob)
+	//Set our entity
+	err = bw.SetEntity(keyblob)
+	if err != nil {
+		fmt.Println("error: ", err.Error())
+		panic(err)
+	}
+	//Publish to our own namespace
+	uri := eVK + "/foo/bar"
+	err = bw.Publish(&bw2bind.PublishParams{
+		URI:            uri,
+		PayloadObjects: []objects.PayloadObject{bw2bind.MkPOString("hello world")},
+		DoVerify : true,
+	})
+	if err != nil {
+		panic(err)
+	}
 }
